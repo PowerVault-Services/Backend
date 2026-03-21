0a12cc9c03dc39ed90043d755479f5a1a2c090fd
SleeperZz <yotsanon2003@gmail.com>
2026-03-21 15:38:56 +0700

    fix: convert jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
-import type { Config } from 'jest';
-
-const config: Config = {
+/** @type {import('jest').Config} */
+module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
@@ -8,5 +7,3 @@ const config: Config = {
   moduleFileExtensions: ['ts', 'js', 'json'],
   clearMocks: true,
 };
-
-export default config;
